62b0ad5f4a8509e3921a1b9578a7608f859c854b
LILJHAY <68815158+LILJHAY@users.noreply.github.com>
2020-08-18 09:16:47 +0000

    Add files via upload

---
+++ HW3_WJY04RL0GC.html/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import NewAssign from './Assign';
+
+import "./css/components.css";
+import "./css/global.css";
+import "./css/layout.css";
+import "./css/sample-wallpaper.css";
+
+
+function App() {
+  return (
+   <NewAssign />
+  );
+}
+
+export default App;
